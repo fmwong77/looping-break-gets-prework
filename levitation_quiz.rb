3c76d663e2fbc50fcf46c03dcb79fb35b2a70c7b
Mei Wong <fongmeiw@gmail.com>
2019-10-08 16:36:59 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,10 +1,17 @@
 
 def levitation_quiz
 	#your code here
-	puts "What is the spell that enacts levitation?"
-	answer = gets.chomp
 	
-	if answer.upcase == "Wingardium Leviosa"
+	loop do
+	  puts "What is the spell that enacts levitation?"
+	  answer = gets.chomp
+	  if answer.upcase == "WINGARDIUM LEVIOSA"
+	    break
+	  end
+	  
+	end
+	
+	puts ""
 end
 
 
